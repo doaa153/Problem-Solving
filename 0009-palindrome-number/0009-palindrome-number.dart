8d7fd194a43099f539847d76cea9461efaa3549c
doaa153 <126961160+doaa153@users.noreply.github.com>
2025-07-11 22:43:34 +0300

    Time: 2 ms (100%), Space: 152.4 MB (44.16%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.dart
@@ -0,0 +1,14 @@
+class Solution {
+  bool isPalindrome(int x) {
+    if (x < 0) return false;
+    int original = x;
+    int reversed = 0;
+    while (x > 0) {
+    reversed = reversed * 10 + x % 10;
+    x ~/= 10;
+  }
+  
+  return original == reversed;
+    
+  }
+}
